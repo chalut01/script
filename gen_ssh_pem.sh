6b6e3fe36470eb1df2d0ba75fcfc84e4822d0606
Chalut Dumrongsiri <jomzsg@hotmail.com>
2020-06-01 17:50:14 +0700

    Update gen_ssh_pem.sh

--- gen_ssh_pem.sh
+++ gen_ssh_pem.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
     #Based on https://linuxaws.wordpress.com/2017/07/17/how-to-generate-pem-file-to-ssh-the-server-without-password-in-linux/
+    # if SSQ edir file /etc/ssh/sshd_config 
+    # line number 22 : AllowUsers ewimaint ansible root
+    # line number 48 : PubkeyAuthentication yes
+    
     user="id_rsa"
     ssh-keygen
     mv $user $user.pem
